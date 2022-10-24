746cf34d53959de1611b83734f421e8030d7a84e
kielsouza <kielsouza115@hotmail.com>
2022-10-24 15:09:24 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,51 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isBtnDisabled: true,
+    };
+  }
+
+  handleChange = (event) => {
+    const minimumLength = 2;
+    const eventValue = event.target.value;
+    if (eventValue.length >= minimumLength) {
+      this.setState({
+        isBtnDisabled: false,
+      });
+    } if (eventValue.length < minimumLength) {
+      this.setState({
+        isBtnDisabled: true,
+      });
+    }
+  };
+
   render() {
+    const { isBtnDisabled } = this.state;
     return (
       <div data-testid="page-search">
         Search
         <Header />
+        <form>
+          <label htmlFor="name-input">
+            Artista:
+            <input
+              data-testid="search-artist-input"
+              id="name-input"
+              type="text"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            disabled={ isBtnDisabled }
+            type="button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
